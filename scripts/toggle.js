3ba66cff70149ec7a22eebcb99cfd9b20303edc2
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2021-12-03 20:54:51 +0400

    Update toggle.js

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -28,7 +28,18 @@ class StateValue {
         }
         return textColor;
     }
-
+    
+    static getToggleIcon() {
+        let toggleIcon;
+        let defaultIcon = `<i class="fas fa-moon fa-2x dark"></i>`;
+        if (!localStorage.getItem('toggleicon')) {
+            toggleIcon = defaultIcon;
+        } else {
+            toggleIcon = localStorage.getItem('toggleicon');
+        }
+        return toggleIcon;
+    }
+    
     static addBgColor(color) {
         let bgColor = StateValue.getBgColor();
         bgColor = color;
@@ -39,6 +50,12 @@ class StateValue {
         let textColor = StateValue.getTextColor();
         textColor = color;
         localStorage.setItem('textcolor', textColor);
+    }
+    
+    static addToggleIcon(icon) {
+        let toggleIcon = StateValue.getToggleIcon();
+        toggleIcon = icon;
+        localStorage.setItem('toggleicon', icon);
     }
 }
 
